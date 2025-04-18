8196fe53f4bda4fcf7c3c0a2c45ede878b3d6c7c
richu <1691401076@qq.com>
2025-04-18 15:26:38 +0800

    chore:add config and change theme

--- app/src/main/java/com/richuff/rcclient/core/design/model/Song.kt
+++ app/src/main/java/com/richuff/rcclient/core/design/model/Song.kt
@@ -1,8 +1,26 @@
 package com.richuff.rcclient.core.design.model
 
+import com.richuff.rcclient.util.Constant
+import kotlinx.serialization.Serializable
 
+
+
+@Serializable
 data class Song(
     val id:Int,
-    val icon:String,
-    val artists: String
+    val icon:String = "",
+    val artists: String = "",
+    val album: String = "",
+    val lyric: String = "",
+    val genre: String,
+    val title:String,
+    val uri:String,
+    val lyricStyle: Int = Constant.VALUE0,
+    val likesCount: Int,
+    val clicksCount: Int,
+    val commentsCount: Int,
+    val userId:String
+
+//    val created: LocalDateTime,
+//    val updated: LocalDateTime,
 )
---
+++ app/src/main/java/com/richuff/rcclient/core/design/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.richuff.rcclient.core.design.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+val largeRound = RoundedCornerShape(
+    topStart = 9.dp,
+    bottomStart = 0.dp,
+    topEnd = 20.dp,
+    bottomEnd = 20.dp
+)
