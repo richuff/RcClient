468ed0d410969f372743ec7e87c188fb64f0973b
richu <1691401076@qq.com>
2025-04-22 22:05:09 +0800

    chore:add new interface

--- app/src/main/java/com/richuff/rcclient/core/network/retrofit/NetworkApiService.kt
+++ app/src/main/java/com/richuff/rcclient/core/network/retrofit/NetworkApiService.kt
@@ -1,5 +1,6 @@
 package com.richuff.rcclient.core.network.retrofit
 
+import com.richuff.rcclient.core.design.model.Sheet
 import com.richuff.rcclient.core.design.model.Song
 import com.richuff.rcclient.core.design.model.response.NetWorkResponse
 import com.richuff.rcclient.core.design.model.response.PageListResponse
@@ -18,4 +19,7 @@ interface NetworkApiService {
 
     @GET("v1/indexes")
     suspend fun indexes(@Query(value = "app") app: Int): NetWorkResponse<PageListResponse<ViewData>>
+
+    @GET("v1/sheets/info")
+    suspend fun sheetDetail(@Query(value = "id")id: String): NetWorkResponse<Sheet>
 }
